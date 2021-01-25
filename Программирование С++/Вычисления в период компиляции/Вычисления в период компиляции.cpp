c25cb2e368cef7c6002b40172213b7ae2b65faba
Roman <65040606+charapennikaurm@users.noreply.github.com>
2021-01-25 17:11:46 +0300

    Add files via upload

---
+++ Программирование С++/Вычисления в период компиляции/Вычисления в период компиляции.cpp
@@ -0,0 +1,25 @@
+﻿//===============================================
+//3.Определить статический массив, размер 
+//которого равен сумме элементов другого массива.
+//===============================================
+
+#include <iostream>
+
+using namespace std;
+
+template<int N>
+constexpr int array_sum(const int(&a)[N])
+{
+	int c = 0;
+	for (int i = 0; i < N; ++i)
+		 c += a[i];
+	return c;
+}
+
+int main()
+{
+	constexpr int a[] = { 1,2,3,4,12,18};
+	int arr[array_sum(a)];
+	cout << _countof(arr);
+	return 0;
+}
